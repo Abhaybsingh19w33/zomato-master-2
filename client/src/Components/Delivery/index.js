3366925737857e5a3c2d0ab391a900f415262c4f
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-10-14 11:02:16 +0530

    added brand carousel to delievery  page

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -2,11 +2,13 @@ import React, { Fragment } from "react";
 
 // components
 import DeliveryCarousal from "./DeliveryCarousel";
+import Brand from "./Brand";
 
 const Delivery = () => {
     return (
         <Fragment>
             <DeliveryCarousal />
+            <Brand />
         </Fragment>
     );
 };
