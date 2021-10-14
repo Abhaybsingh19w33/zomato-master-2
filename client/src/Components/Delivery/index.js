81d97aee4fb6714c608875fc254b989e1e27bf45
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-10-14 11:00:12 +0530

    Added deliveryCarousel using delievery category to create food template

---
+++ client/src/Components/Delivery/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+// components
+import DeliveryCarousal from "./DeliveryCarousel";
+
+const Delivery = () => {
+    return (
+        <Fragment>
+            <DeliveryCarousal />
+        </Fragment>
+    );
+};
+
+export default Delivery;
--- client/src/Components/master.js
+++ client/src/Components/master.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
 import { useParams } from "react-router-dom";
 
+// components
+import Delivery from "./Delivery";
+
 const Master = () => {
     const { type } = useParams();
 
-    return <Fragment>{type}</Fragment>;
+    return <div className="my-5">
+        {type === "delivery" && <Delivery />}
+    </div>;
 };
 
 export default Master;
