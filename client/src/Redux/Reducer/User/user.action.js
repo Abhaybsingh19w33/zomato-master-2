0b364ab7c8f5583fec766b9c455719fbcd1cb73d
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-10-15 00:57:21 +0530

    add auth

--- client/src/Redux/Reducer/User/user.action.js
+++ client/src/Redux/Reducer/User/user.action.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Redux types
-import { GET_USER, SELF } from "./user.type";
+import { GET_USER, SELF, CLEAR_USER } from "./user.type";
 
 export const getUser = (_id) => async (dispatch) => {
     try {
@@ -15,6 +15,15 @@ export const getUser = (_id) => async (dispatch) => {
         return dispatch({ type: "ERROR", payload: error });
     }
 };
+
+export const clearUser = () => async (dispatch) => {
+    try {
+        return dispatch({ type: CLEAR_USER, payload: {} });
+    } catch (error) {
+        return dispatch({ type: "ERROR", payload: error });
+    }
+};
+
 export const getMyself = () => async (dispatch) => {
     try {
         const User = await axios({
