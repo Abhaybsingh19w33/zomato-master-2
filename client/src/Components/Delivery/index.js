f74ce4e35facf86cbe185016b06b58b88f58d640
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-10-14 12:31:57 +0530

    Added Restaurant Card

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -3,12 +3,14 @@ import React, { Fragment } from "react";
 // components
 import DeliveryCarousal from "./DeliveryCarousel";
 import Brand from "./Brand";
+import RestaurantCard from "../RestaurantCard";
 
 const Delivery = () => {
     return (
         <Fragment>
             <DeliveryCarousal />
-            <Brand />
+            {/* <Brand /> */}
+            <RestaurantCard />
         </Fragment>
     );
 };
